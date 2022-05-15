c922f08aae922ef77b86918b61e1a9f34331d516
Rahul Badgujar <rahulbadgujar1508@gmail.com>
2022-05-16 02:41:42 +0530

    added bloc observer

--- lib/auth_bloc/login_cubit.dart
+++ lib/auth_bloc/login_cubit.dart
@@ -1,7 +1,21 @@
 import 'package:bloc/bloc.dart';
+import 'package:login_page_bloc/auth/email_password_login.dart';
+import 'package:login_page_bloc/auth/email_password_login_postgres_impl.dart';
 
 import 'auth_states.dart';
 
 class LoginCubit extends Cubit<LoginState> {
+  // chosing postgres auth for login here, can change from here
+  final EmailPasswordLogin loginService = EmailPasswordLoginPostgresImpl();
+
   LoginCubit() : super(LoginIdle());
+
+  Future<void> login({required String email, required String password}) async {
+    try {
+      await loginService.login(email: email, password: password);
+      emit(LoginSuccessfull());
+    } on Exception catch (e) {
+      emit(LoginFailed(message: e.toString()));
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:bloc/bloc.dart';
 import 'package:flutter/material.dart';
 
 import 'app.dart';
+import 'auth_bloc/auth_bloc_observer.dart';
 
 void main() {
-  runApp(const App());
+  BlocOverrides.runZoned(
+    () => runApp(const App()),
+    blocObserver: AuthBlocObserver(),
+  );
 }
